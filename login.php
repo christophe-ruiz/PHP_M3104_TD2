de701209067755baaec4f6c0e8bb291b79422826
Christophe Ruiz <christophe.ruiz@etu.univ-amu.fr>
2019-09-16 21:21:26 +0200

    test pass

--- login.php
+++ login.php
@@ -1,7 +1,10 @@
 <?php
+    include 'utils.inc.php';
+    start_page('Pass');
 ?>
 <form method="POST" action="test-pass.php">
     <input type="text" name="login">
     <input type="password" name="pwd">
     <button name="action" type="submit"></button>
 </form>
+<?php end_page(); ?>
