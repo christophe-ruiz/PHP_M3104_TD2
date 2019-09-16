e599948f550410a1ab3d36d327d0edbc94b6abaa
Christophe Ruiz <christophe.ruiz@etu.univ-amu.fr>
2019-09-16 21:24:36 +0200

    test pass

--- test-pass.php
+++ test-pass.php
@@ -1,8 +1,8 @@
 <?php
     include 'utils.inc.php';
-    if (isset($_POST['action'])) {
-        $login = $_POST['login'];
-        $pwd = $_POST['pwd'];
+    $login = $_POST['login'];
+    $pwd = $_POST['pwd'];
+    if (isset($_POST['action']) && $login == 'Jacques' && $pwd == 'azerty123') {
         start_page('Bienvenue');
         echo '<h1> Bienvenue ' . $login . '</h1>' . PHP_EOL;
         end_page();
