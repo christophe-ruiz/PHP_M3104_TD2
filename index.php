d099bd0bb672837b136e359bdee301ccf70d91d9
r18000643 <christophe.ruiz@etu.univ-amu.fr>
2019-09-16 14:26:46 +0200

    formulaire sauvage

--- index.php
+++ index.php
@@ -1,4 +1,29 @@
 <?php
     include 'utils.inc.php';
-    phpinfo();
 ?>
+<!DOCTYPE html>
+<html>
+<body>
+    <form>
+        <input type="text" name="id" placeholder="Identifiant">
+        <input type="radio" id="male" name="male">
+        <label for="male">Homme</label>
+        <input type="radio" name="female">
+        <label for="female">Femme</label>
+        <input type="text" name="mail" placeholder="E-mail">
+        <input type="password" name="pwd" placeholder="Mot de passe">
+        <input type="password" name="verifPwd" placeholder="Vérifiez le mdp">
+        <input type="text" name="phone" placeholder="Téléphone">
+        <select id="pays">
+            <option value="">Selectionner Pays</option>
+            <option value="france" name="fr">France</option>
+            <option value="belgique" name="bg">Belgique</option>
+            <option value="monaco" name="mn">Monaco</option>
+            <option value="autre" name="autre">Autre</option>
+        </select>
+        <input type="checkbox" id="cgu" name="cgu">
+        <label for="cgu">J'accepte les Conditions Générales d'Utilisation</label>
+        <input type="submit" value="Envoyer">
+    </form>
+</body>
+</html>
