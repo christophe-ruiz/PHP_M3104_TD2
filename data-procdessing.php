dd03e2e309fa33276cefc71394875c17dc094f9d
r18000643 <christophe.ruiz@etu.univ-amu.fr>
2019-09-16 14:44:34 +0200

    formulaire sauvage

---
+++ data-procdessing.php
@@ -0,0 +1,17 @@
+<?php
+    echo $_POST['pays'];
+/*
+    $action = $_POST['action'];
+    if ($action == 'Mailer') {
+        $id = $_POST['id'];
+        if (isset($_POST['male'])) $sexe = $_POST['male'];
+        else if (isset($_POST['female'])) $sexe = $_POST['female'];
+        $email = $_POST['mail'];
+        $pwd = $_POST['pwd'];
+        $verifPwd = $_POST['verifPwd'];
+        $phone = $_POST['phone'];
+        $pays = $_POST['pa'];
+    } else {
+        echo '<br/><strong>Bouton non géré !</strong><br/>';
+    }
+*/
