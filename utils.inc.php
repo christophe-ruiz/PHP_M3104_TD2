7040f31db974db009ec3bc0cba5856025a37ee00
Christophe Ruiz <christophe.ruiz@etu.univ-amu.fr>
2019-09-16 21:29:06 +0200

    test pass

--- utils.inc.php
+++ utils.inc.php
@@ -1,9 +1,10 @@
 <?php
     function start_page ($title)
     {
-        echo '<!DOCTYPE html> <html lang=fr>
-                  <head>
-                    <title>' . PHP_EOL . $title . '</title>
+        echo '<!DOCTYPE html> ' . PHP_EOL .
+             '<html lang=fr> ' . PHP_EOL .
+             '<head>
+                    <title>' . $title . '</title>
                   </head>
                   <body>' . PHP_EOL;
     };
